70d33181edcc8ff63348c062711cac02ab743305
AbuBackker <abubackker11@gmail.com>
2020-08-04 21:33:58 +0600

    added function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Letter Grade
+// Letter Grade =>
 
 // var marks= prompt("Please enter your Number:");
 
@@ -27,8 +27,7 @@
 // 	document.write("<h1>Fail</h1>");
 // }
 
-
-// Vowel / Consonant
+// Vowel / Consonant =>
 
 // var letter= prompt("Please enter your Number:");
 
@@ -41,8 +40,7 @@
 // 	document.write("<h1>Cosonant</h1>");
 // }
 
-
-// Switch, Case, Break, Default
+// Switch, Case, Break, Default =>
 
 // var letter= prompt("Please enter your Letter:");
 
@@ -52,7 +50,7 @@
 // 	case "a":
 // 	case "e":
 // 	case "i":
-// 	case "o": 
+// 	case "o":
 // 	case "u":
 // 	document.write("<h1>Vowel</h1>");
 // 	break;
@@ -61,16 +59,13 @@
 
 // }
 
+// For Loop =>
 
-// For Loop
+for (var i = 100; i >= 1; i = i - 2) {
+    // document.write(i+"</br>");
+}
 
-// for (var i = 100; i >= 1; i=i-2)
-// {
-// 	document.write(i+"</br>");
-// }
-
-
-// 1+2+3...
+// 1+2+3...+N
 
 // var first = parseInt(prompt("Enter Your First Number:"));
 // var second = parseInt(prompt("Enter Your Second Number:"));
@@ -83,11 +78,92 @@
 
 // document.write(sum);
 
-
 // Adding two number for 3 times
-for (var i = 0; i < 3; i++) {
-var num1 = parseInt(prompt("Enter Your First Number:"));
-var num2 = parseInt(prompt("Enter Your Second Number:"));
-var sum = num1 + num2;
-document.write(sum+"</br>");
+
+// for (var i = 0; i < 3; i++) {
+// var num1 = parseInt(prompt("Enter Your First Number:"));
+// var num2 = parseInt(prompt("Enter Your Second Number:"));
+// var sum = num1 + num2;
+// document.write(sum+"</br>");
+// }
+
+// While Loop =>
+
+var i = 1;
+while (i <= 100) {
+    // document.write(i+"</br>");
+    i++;
 }
+
+// 1+2+3+4+5=?
+
+var i = 1;
+var sum = 0;
+
+while (i <= 5) {
+    sum = sum + i;
+    i++;
+}
+// document.write(sum);
+
+// 2 + 4 + 6 + ... + 100
+
+var i = 2;
+var sum = 0;
+while (i <= 100) {
+    sum = sum + i;
+    i = i + 2;
+}
+// document.write(sum);
+
+// Divisible by 3 & 5 for ( 1 to 100 ) =>
+
+var i = 1;
+var sum = 0;
+while (i <= 100) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        sum = sum + i;
+    }
+    i++;
+}
+// document.write(sum);
+
+// Do While Loop =>
+
+var i = 1;
+do {
+    // document.write(" "+ i);
+    i++;
+} while (i <= 10);
+
+// Break & Continue =>
+
+for (var i = 0; i <= 100; i++) {
+    if (i == 10) {
+        break;
+    }
+
+    // document.write(" "+ i);
+}
+
+for (var i = 0; i <= 100; i++) {
+    if (i == 10) {
+        continue;
+    }
+
+    // document.write(" " + i);
+}
+
+// Function =>
+
+function square(num1) {
+    var result = num1 * num1;
+    document.write("Result = " + result);
+}
+// square(5);
+
+function addition(num1, num2) {
+    var result = num1 + num2;
+    return result;
+}
+// document.write("Result = " + addition(5, 4));
